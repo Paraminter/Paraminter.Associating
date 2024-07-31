ca0f50ad06cad6532928add0b1b5774642847a37
ErikWe <erikwestberg7@gmail.com>
2024-07-31 17:40:31 +0200

    IAssociateArguments as command, not query

---
+++ src/Commands/IAssociateArgumentsCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Paraminter.Associators.Commands;
+
+using Paraminter.Commands;
+
+/// <summary>Represents a command to associate arguments with parameters.</summary>
+/// <typeparam name="TData">The type representing the data used to associate arguments with parameters.</typeparam>
+public interface IAssociateArgumentsCommand<out TData>
+    : ICommand
+{
+    /// <summary>The data used to associate arguments with parameters.</summary>
+    public abstract TData Data { get; }
+}
