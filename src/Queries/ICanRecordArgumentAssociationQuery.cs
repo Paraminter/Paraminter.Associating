6e9de187c48dd61491c4d4a21cadd366a59cea94
ErikWe <erikwestberg7@gmail.com>
2024-08-02 23:35:00 +0200

    Rm CanRecordQuery

--- src/Queries/ICanRecordArgumentAssociationQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Paraminter.Associators.Queries;
-
-using Paraminter.Queries;
-
-/// <summary>Represents a query for whether an association between arguments and parameters can be recorded.</summary>
-public interface ICanRecordArgumentAssociationQuery
-    : IQuery
-{ }
--- src/Queries/README.nupkg.md
+++
@@ -1,5 +0,0 @@
-# Paraminter.Associators.Queries
-
-Defines queries related to `Paraminter` associators.
-
-See [GitHub](https://github.com/Paraminter/Paraminter.Associators) for more information.
